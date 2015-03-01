a11d6bbf1dae7c481fe9df7328f4eaa193d8c60b
jejaeddy <jeja12@live.com>
2015-02-28 19:15:31 -0600

    Update Puente.java
    Javadoc

--- Puente.java
+++ Puente.java
@@ -11,21 +11,35 @@ public class Puente {
     private static ListaSimple <Puente> puentes = new ListaSimple();
     private static ListaSimple <Image> puentesG = new ListaSimple();
     
+    /**
+     * Metodo constructor de Puente
+     */
     public Puente(){
         Random random = new Random();
         //La vida aleatoria con una media de 15
         vida = (int)(15 + 2*random.nextGaussian());
     }//Fin del constructor
     
+    /**
+     * Metodo devuelve la lista de puentes
+     * @return puentes
+     */
     public static ListaSimple getLista(){
         return puentes;
     }//Fin de getLista
     
-    
+    /**
+     * Metodo devuelve la lista grafica de puentes
+     * @return puentesG
+     */
     public static ListaSimple getListaGrafica(){
         return puentesG;
     }//Fin de getLista
     
+    /**
+     * Metodo define el daño al puente
+     * @param daño 
+     */
     public void setVida(int daño){
         vida = vida - daño;
         if(vida <= 0){
@@ -42,4 +56,4 @@ public class Puente {
         return vida;
     }//Fin de getVida
     
-}//Fin de la clase
+}//Fin de Puente
